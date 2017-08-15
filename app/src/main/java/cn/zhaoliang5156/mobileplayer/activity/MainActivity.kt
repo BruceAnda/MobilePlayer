7c27622b08f5ec66ea22fe1578abe26dc79158f4
zhaoliang <2668645098@qq.com>
2017-08-15 09:51:39 +0800

    初始化项目

---
+++ app/src/main/java/cn/zhaoliang5156/mobileplayer/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package cn.zhaoliang5156.mobileplayer.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import cn.zhaoliang5156.mobileplayer.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
